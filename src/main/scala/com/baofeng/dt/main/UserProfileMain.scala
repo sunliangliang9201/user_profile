f82e91983c56c44a2d92ac1549bede3e55cc28b1
sunliangliang <397585361@qq.com>
2019-04-26 19:44:33 +0800

    'init'

--- src/main/scala/com/baofeng/dt/main/UserProfileMain.scala
+++ src/main/scala/com/baofeng/dt/main/UserProfileMain.scala
@@ -1,9 +1,9 @@
 package com.baofeng.dt.main
 
 /**
-  * 暴风用户画像主程序入口，数据来源主要以wirless、pc、tv埋点数据以及线上用户信息数据库，以及第三方用户信息
+  * 用户画像主程序入口，数据来源主要以wirless、pc、tv埋点数据以及线上用户信息数据库，以及第三方用户信息
   * 主要流程为：数据收集、处理 —— 模型构建（基于规则 + 机器学习） —— 打用户标签 —— 以规定格式存储
-  * 具体细节待调研完毕再详细注解
+  * 具体细节待调研完毕后整理到README.md
   *
   * @author sunliangliang 2019/4/26 https://github.com/sunliangliang9201/user_profile
   * @version 1.0
